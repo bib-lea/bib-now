ed794a8e30b615320c513fc8d311de22b3404e6c
Jay Kim <jawoon.kim@edu.bib.de>
2020-09-14 10:34:15 +0100

    Added MatModules

--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
+const MatModules = [
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    MatModules
+  ],
+  exports: [
+    MatModules
   ]
 })
 export class MaterialModule { }
