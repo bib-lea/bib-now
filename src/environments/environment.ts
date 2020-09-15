9d07a7584234ae8c5128d0739158035909d544b2
Charlie <charlie.oliver.wiegand@gmail.com>
2020-09-15 09:00:04 +0200

    moved firebase config to testing environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyDxes0zBq2k6nz1tmQtMnSPe9PimXOH694",
+  authDomain: "bibnow-testing.firebaseapp.com",
+  databaseURL: "https://bibnow-testing.firebaseio.com",
+  projectId: "bibnow-testing",
+  storageBucket: "bibnow-testing.appspot.com",
+  messagingSenderId: "101574135416",
+  appId: "1:101574135416:web:9c8073ed4f34e45ac9e78d",
+  measurementId: "G-4PB9C5HP69"}  
 };
 
 /*
