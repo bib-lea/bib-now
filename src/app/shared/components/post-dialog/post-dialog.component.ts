e115a485bf16d1873fb38a6e879984d61fd44f48
Jay Kim <jawoon.kim@edu.bib.de>
2020-09-16 13:44:59 +0100

    Post function added

--- src/app/shared/components/post-dialog/post-dialog.component.ts
+++ src/app/shared/components/post-dialog/post-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {FormControl} from '@angular/forms';
+import {CrudService} from '../../services/crud.service';
+import {Post} from '../../models/post';
 
 @Component({
   selector: 'app-post-dialog',
@@ -7,10 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDialogComponent implements OnInit {
 
-  constructor(
-  ) { }
+  contentControl: FormControl;
+  user: any;
+  posts: Post[];
 
-  ngOnInit(): void {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private crudService: CrudService
+  )
+  {
+    this.afAuth.onAuthStateChanged(user => {
+      this.user = user;
+      console.log(this.user);
+    });
   }
 
+  ngOnInit(): void {
+    this.contentControl = new FormControl('', []);
+  }
+
+  onPosten(): void {
+    let post: Post = {
+      username: this.user ? this.user.email : 'Anonym',
+      content: this.contentControl.value
+    };
+
+    this.crudService.createPost(post)
+      .then(res => {
+        console.log(res);
+        this.resetControl();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  private resetControl(): void {
+    this.contentControl.reset();
+  }
 }
