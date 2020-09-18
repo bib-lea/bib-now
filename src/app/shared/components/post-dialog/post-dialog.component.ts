1e4c55173d159f46b7fb6f575043c9727d6ea30f
Charlie <charlie.oliver.wiegand@gmail.com>
2020-09-18 10:45:53 +0200

    added new post changes to actual ui

--- src/app/shared/components/post-dialog/post-dialog.component.ts
+++ src/app/shared/components/post-dialog/post-dialog.component.ts
@@ -3,6 +3,10 @@ import {AngularFireAuth} from '@angular/fire/auth';
 import {FormControl} from '@angular/forms';
 import {CrudService} from '../../services/crud.service';
 import {Post} from '../../models/post';
+import { Observable } from "rxjs";
+import { AngularFireStorage} from "@angular/fire/storage";
+import { finalize } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-post-dialog',
@@ -14,10 +18,17 @@ export class PostDialogComponent implements OnInit {
   contentControl: FormControl;
   user: any;
   posts: Post[];
+  downloadURL: Observable<string>;
+
+
+  image: string
+  topic: FormControl;
+  content: FormControl;
 
   constructor(
     private afAuth: AngularFireAuth,
-    private crudService: CrudService
+    private crudService: CrudService,
+    private storage: AngularFireStorage
   )
   {
     this.afAuth.onAuthStateChanged(user => {
@@ -29,6 +40,50 @@ export class PostDialogComponent implements OnInit {
   ngOnInit(): void {
     this.contentControl = new FormControl('', []);
   }
+
+  onPosten() {
+    const postData = {
+      //author: this.auth.authState.displayName || this.auth.authState.email,
+      author: '',
+      //authorId: this.auth.currentUserId,
+      authorId: '',
+      content: this.content.value,
+      image: this.image || null,
+      published: new Date(),
+      topic: this.topic.value
+    }
+    this.crudService.createPost(postData).then(res => {
+      this.topic.reset();
+      this.content.reset();
+      this.image = '';
+      console.log(res);
+    })}
+
+    upload(event) {
+      var n = Date.now();
+      const file = event.target.files[0];
+      const filePath = `posts/${n}`;
+      const fileRef = this.storage.ref(filePath);
+      const task = this.storage.upload(`posts/${n}`, file);
+      task
+        .snapshotChanges()
+        .pipe(
+          finalize(() => {
+            this.downloadURL = fileRef.getDownloadURL();
+            this.downloadURL.subscribe(url => {
+              if (url) {
+                this.image = url;
+              }
+              console.log(this.image);
+            });
+          })
+        )
+        .subscribe(url => {
+          if (url) {
+            console.log(url);
+          }
+        });
+      }
 
   // onPosten(): void {
   //   let post: Post = {
