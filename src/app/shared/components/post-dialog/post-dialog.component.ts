6b4745636c2f9aca250eaf9bfa1293c5f43e9511
Jay Kim <jawoon.kim@edu.bib.de>
2020-09-21 14:04:37 +0100

    Resolved Conflicts

--- src/app/shared/components/post-dialog/post-dialog.component.ts
+++ src/app/shared/components/post-dialog/post-dialog.component.ts
@@ -7,11 +7,8 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { Observable } from "rxjs";
 import { AngularFireStorage} from "@angular/fire/storage";
 import { finalize } from 'rxjs/operators';
-<<<<<<< HEAD
 import {DEFAULT_IMAGE_URL} from '../../constants/constants';
-=======
 import { AuthServiceService } from "../../services/auth-service.service";
->>>>>>> f1ba86a7f87c3ca27d76ab5b759e5b942f55ced8
 
 @Component({
   selector: 'app-post-dialog',
